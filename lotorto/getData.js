1bde18f4218ca0c63972e756c3d84c9c5859c11f
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2025-03-28 12:32:06 +0000

    punto 2

--- lotorto/getData.js
+++ lotorto/getData.js
@@ -1,13 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
 const fs = require('fs');
 const axios = require('axios');
 
 const url = 'https://randomuser.me/api/?results=5&inc=gender,name,nat,id,city,picture,nat';
 
-function getData () {
-  axios.get(url)
+axios.get(url)
   .then(response => {
     const users = response.data.results;
     fs.writeFileSync('users.json', JSON.stringify(users, null, 2), 'utf-8');
@@ -15,13 +11,4 @@ function getData () {
   })
   .catch(error => {
     console.error('Errore nel recupero dei dati:', error);
-  });
-  return users;
-}
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  
 });
-
-module.exports = router;
