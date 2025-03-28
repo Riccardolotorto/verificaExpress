94ed5b32f8b76e2b6fa1f7dfa9420b213c16b3a1
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2025-03-28 12:53:53 +0000

    punto 5

--- lotorto/routes/users.js
+++ lotorto/routes/users.js
@@ -6,8 +6,19 @@ const router = express.Router();
 const users = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
 
 // Route per ottenere tutti gli utenti
-router.get('/rest', (req, res) => {
+router.get('/', (req, res) => {
   res.json(users);
+});
+
+// Route per ottenere un utente specifico tramite email
+router.get('/users/:email', (req, res) => {
+  const user = users.find(u => u.email === req.params.email);
+
+  if (!user) {
+    return res.status(404).json({ error: 'Utente non trovato' });
+  }
+
+  res.json(user);
 });
 
 module.exports = router;
